f9f95133757cef9e8fd92e7fb2264727fb56d145
austen.shelton <austen.shelton@gmail.com>
2021-09-30 08:51:05 -0400

    js and html edits

--- js/main.js
+++ js/main.js
@@ -3,22 +3,28 @@ var kelvinDiv = document.getElementById("kelvin");
 var celciusDiv = document.getElementById("celcius");
 var farenheitDiv = document.getElementById("celcius");
 var conditionsDiv = document.getElementById("conditions");
-var mainDiv = document.getElementById("main");
+var container2Div = document.getElementById("container2");
+var container3Div = document.getElementById("container3");
+var container4Div = document.getElementById("container4");
 var errorDiv = document.getElementById("error");
-
+var imageDiv = document.getElementById("image");
 
 function setZipCode(value) {
     zipCode = value
 }
 
 function start() {
-    mainDiv.style.display = "none";
     errorDiv.style.display = "none";
+    container2Div.style.display = "none";
+    container3Div.style.display = "none";
+    container4Div.style.display = "none";
 }
 
 function updateWeather() {
     errorDiv.style.display = "none";
-    mainDiv.style.display = "block";
+    container2Div.style.display = "block";
+    container3Div.style.display = "block";
+    container4Div.style.display = "block";
     cityDiv.innerText = place;
     kelvinDiv.innerText = temperature;
     celciusDiv.innerText = Math.round(temperature - 273.15);
@@ -26,7 +32,9 @@ function updateWeather() {
 }
 
 function wrongZip() {
-    mainDiv.style.display = "none";
+    container2Div.style.display = "none";
+    container3Div.style.display = "none";
+    container4Div.style.display = "none";
     errorDiv.style.display = "block";
 }
 var api = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=4af16e039a66300f9ce07ec031c6035e";
@@ -47,3 +55,5 @@ axios.get(api)
     // handle error
     console.log(error);
   })
+
+  document.onload = start;
